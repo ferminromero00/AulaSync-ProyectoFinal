f95fea8461cd58e56ddc0e95a7060f41989301e5
Fermin <ferminromero2005@gmail.com>
2025-05-25 11:03:49 +0200

    Cambios...

---
+++ AulaSyncSymfony/src/Controller/Api/TareaController.php
@@ -0,0 +1,12 @@
+<?php
+
+// Elimina este archivo si no contiene una clase válida.
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TareaController extends AbstractController
+{
+    // ...implementación...
+}
