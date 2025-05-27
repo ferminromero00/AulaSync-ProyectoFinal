c5ed7360bd2ffe27a520cba0b78a26529b4351b2
Fermin <ferminromero2005@gmail.com>
2025-05-27 22:33:13 +0200

    Pequeños cambios en terraform...

--- terraform/Grupos_Seguridad.tf
+++ terraform/Grupos_Seguridad.tf
@@ -11,16 +11,6 @@ resource "aws_security_group" "grupo_seguridad_servidor" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Acceso HTTP desde cualquier origen"
   }
-
- # Regla de entrada para HTTP en puerto 8080
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Acceso HTTP desde cualquier origen en puerto 8080"
-  }
-
   # Regla de entrada para SSH
   ingress {
     from_port   = 22
@@ -29,16 +19,47 @@ resource "aws_security_group" "grupo_seguridad_servidor" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Acceso SSH desde cualquier origen"
   }
-
-  # Regla de entrada para HTTP en puerto 8000 (Symfony API)
   ingress {
-    from_port   = 8000
-    to_port     = 8000
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Acceso API Symfony"
+    description = "Acceso HTTPS desde cualquier origen"
   }
 
+  # Regla de entrada para NFS (EFS)
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Acceso a EFS desde cualquier origen"
+  }
+
+  # Comunicación entre nodos
+  ingress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "-1"
+    self      = true
+  }
+
+  # Acceso a la API de Kubernetes (puerto 6443)
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Metrics Server (puerto 4443)
+  ingress {
+    from_port   = 4443
+    to_port     = 4443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   # Regla de salida para todo el trafico
   egress {
     from_port   = 0
@@ -50,61 +71,5 @@ resource "aws_security_group" "grupo_seguridad_servidor" {
 
   tags = {
     Name = "GrupoSeguridadServidor"
-  }
-}
-
-resource "aws_security_group" "grupo_seguridad_eks" {
-  vpc_id      = aws_vpc.principal.id
-  name        = "grupo-seguridad-eks"
-  description = "Grupo de seguridad para EKS"
-
-  # Permitir todo el tráfico entre nodos del cluster
-  # Regla de entrada para HTTP
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Acceso HTTP desde cualquier origen"
-  }
-
- # Regla de entrada para HTTP en puerto 8080
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Acceso HTTP desde cualquier origen en puerto 8080"
-  }
-
-  # Regla de entrada para SSH
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Acceso SSH desde cualquier origen"
-  }
-
-  # Regla de entrada para HTTP en puerto 8000 (Symfony API)
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Acceso API Symfony"
-  }
-
-  # Regla de salida para todo el trafico
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Permite todo el trafico de salida"
-  }
-
-  tags = {
-    Name = "GrupoSeguridadEKS"
   }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,8 @@ provider "aws" {
 # VPC Principal
 resource "aws_vpc" "principal" {
   cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
     Name = "VPCPrincipal"
   }
